10a118b3e1cc31cfc5728da0c45d7751837d6e41
Nick Pisacane <pisacanen@gmail.com>
2017-01-01 12:56:55 -0800

    initial graphql tests

--- test/server/graphql/graphql.test.js
+++ test/server/graphql/graphql.test.js
@@ -1,3 +1,38 @@
+import path from 'path'
 import test from 'ava'
+import { graphql } from 'graphql'
 
-test.todo('write graphql tests')
+import schema from '../../../src/server/graphql'
+import config from  '../../../src/config'
+
+const testFixtures = path.resolve(
+  path.join(__dirname, '..', '..', 'test_fixtures')
+)
+const q = query => graphql(schema, query)
+config.merge({
+  context: testFixtures,
+  staticBaseURL: '/static'
+})
+
+test('Root Node query', async t => {
+  const { data: { node } } = await q(`{
+    node {
+      isDirectory
+      exists
+      dirname
+      basename
+      ... on Directory {
+        children {
+          basename
+        }
+      }
+    }
+  }`)
+
+  t.is(node.isDirectory, true)
+  t.is(node.exists, true)
+  t.is(node.dirname, '/')
+  t.is(node.basename, '')
+  t.truthy(node.children)
+  t.is(node.children.length, 2)
+})
