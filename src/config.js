3751b939164c17a6b2ebe061a938588375bc3ed5
Nick Pisacane <pisacanen@gmail.com>
2017-01-01 19:05:01 -0800

    support private/immutable configuration

--- src/config.js
+++ src/config.js
@@ -3,19 +3,25 @@ import path from 'path'
 let config = {
   FS: 'LOCAL_FS',
   context: path.resolve(path.join(__dirname, '..', '..', 'fixtures')),
-  staticBaseURL: '/static',
-  internalStaticURL: '__internal__',
-  publicStaticURL: '__static__'
+  staticBaseURL: '/__static__',
+  port: 8000
+}
+
+const privateConfig = {
+  internalBaseURL: '/__internal__'
 }
 
 export default {
   set (prop, value) {
+    if (privateConfig.hasOwnProperty(prop)) {
+      throw new Error(`Property: \`${prop}\` is unconfigurable.`)
+    }
     config[prop] = value
   },
 
   get (prop) {
-    if (!prop) return Object.assign({}, config)
-    return config[prop]
+    if (!prop) return Object.assign({}, config, privateConfig)
+    return config.hasOwnProperty(prop) ? config[prop] : privateConfig[prop]
   },
 
   merge (opts = {}) {
